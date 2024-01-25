0bdd4ef91d21bb682a6fecd7ae223faacc9640cd
Paula Cano <paulacanoroman@gmail.com>
2024-01-25 23:34:30 +0100

    feat: interface and function createTodo

--- index.ts
+++ index.ts
@@ -1,3 +1,11 @@
+
+interface Todo {
+    text: string,
+    completed: boolean,
+}
+
+const todos: Todo[] = [];
+
 const btn = document.getElementById("btn")! as HTMLButtonElement; // ! añadir ! Es un operador tipográfico. Lo pones después de alguna expresión que podría ser null y TypeScript asumirá que no es null. que 
 const input = document.getElementById("todoinput")! as HTMLInputElement; 
 const form = document.querySelector("form")!;
@@ -12,15 +20,24 @@ const list = document.getElementById("todolist")!;
 // lo mismo que: 
 function handleSubmit(e: SubmitEvent){
         e.preventDefault();
-        const newToDoText = input.value;
-        const newLI = document.createElement("LI");
-        const checkbox = document.createElement("input");
-        checkbox.type = "checkbox";
-        newLI.append(checkbox)
-        newLI.append(newToDoText);
-        list?.append(newLI); // por si es nulo
+        const newTodo: Todo = {
+            text: input.value,
+            completed: false,
+        };
+        createTodo(newTodo);
+        todos.push(newTodo);
         input.value = "";
     }
+
+function createTodo (todo: Todo) {
+        const newLI = document.createElement("li");
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        newLI.append(todo.text);
+        newLI.append(checkbox)
+        list?.append(newLI); // por si es nulo
+
+}
 
 form.addEventListener("submit", handleSubmit)
 
